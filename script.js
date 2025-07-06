54d6641be12a6ae81832054f0a477e474f50f781
Gabriel <gabrielboetto@hotmail.com>
2025-07-06 17:23:31 -0300

    js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ document.getElementById('formulario').addEventListener('submit', function (e) {
     e.preventDefault();
     alert("¡Gracias por tu interés! Te contactaremos pronto.");
   });
-  
+  const colorSelect = document.getElementById('color-select');
+const productImage = document.getElementById('product-image');
+
+colorSelect.addEventListener('change', () => {
+  const selectedImage = colorSelect.value;
+  productImage.src = `./multimedia/${selectedImage}`;
+});
